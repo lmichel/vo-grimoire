1fc672242b6c903fb21a8bcfbfbccd53dfbcb390
cisterni2u <enzo.cisternino6@etu.univ-lorraine.fr>
2020-04-16 14:40:09 +0200

    16/04 : Ajout de commentaire/Création de petits jeu de données en JSON/Chemins relatifs pour les téléchargements et sauvegardes

--- sandbox/python/testPerceval/classes/testPerceval.py
+++ sandbox/python/testPerceval/classes/testPerceval.py
@@ -6,12 +6,24 @@ from os import path
 
 from perceval.backends.core.mbox import MBox
 
+'''
+This program is build to test GrimoireLab : Perceval and especially Perceval on .mbox files
+You will be able to :
+    - Display all the messages of an archive
+    - Display the number of mails sent by someone
+    - Display the first five mails of the archive
+    - Search a keyword in the archive
+    - Build a JSON with N messages
+'''
+
 class analyzePerceval(object):
 
+    # Method who download the archive and stores it in ../data/mbox
+    # If the archive is already downloaded, it will not redownload-it
+    # The path is relative
     def downloadMails(self):
         base_path = os.path.dirname(os.path.realpath(__file__))
-        print(base_path)
-        mbox_path = base_path.replace("/python/testPerceval/classes","/data/mbox")
+        mbox_path = base_path.replace("/python/testPerceval/classes", "/data/mbox")
         if path.exists(mbox_path + '/mails.mbox') is False:
             print("\nBeginning Download...")
             url = 'http://mail.ivoa.net/pipermail/dm.mbox/dm.mbox'
@@ -19,19 +31,24 @@ class analyzePerceval(object):
             print("\nDownload Finished !")
         else:
             print("\nArchive already downloaded !")
+        print("\nThe archive is located at : " + mbox_path)
         return mbox_path
 
+    # This method allow you to see in the terminal the progress of the download
     def showProgress(self, count, bloc_size, total_size):
         percent = int(count * bloc_size * 100 / total_size)
         sys.stdout.write("\r" + "...%d%%" % percent)
 
-    def createRepo(self,dir):
-        print(dir)
+    # This method create the repository for Perceval to work
+    # You will need to specify the target mailing list
+    # and the path where the archives are
+    def createRepo(self, dir):
         mbox_uri = 'dm@iova.net'
         mbox_dir = dir
         repo = MBox(uri=mbox_uri, dirpath=mbox_dir)
         return repo
 
+    # Simple method to type the Name of a person
     def typeName(self):
         name = input("\nType the Name of the person : ")
         if name is None:
@@ -40,6 +57,7 @@ class analyzePerceval(object):
         else:
             return name
 
+    # Simple method to type words
     def typeWord(self):
         word = input("\nType the keyword : ")
         if word is None:
@@ -48,11 +66,13 @@ class analyzePerceval(object):
         else:
             return word
 
+    # Simple method who prints all the messages of an archive
     def getAllMessages(self, repo):
         print("\nWORK IN PROGRESS !")
         for message in repo.fetch():
             print(json.dumps(message, indent=4))
 
+    # This method counts the number of mails sent by the person with his name passed in parameter
     def getMessageCounterName(self, repo, nom):
         print("\nWORK IN PROGRESS !")
         compteur = 0
@@ -64,6 +84,7 @@ class analyzePerceval(object):
         except:
             print('\n')
 
+    # This method print all the messages with the specified keyword in it
     def keyword(self, repo, keyword):
         try:
             for message in repo.fetch():
@@ -72,6 +93,7 @@ class analyzePerceval(object):
         except:
             print('\n')
 
+    # This method build an array with the first five messages of the archive
     def divideMbox(self, repo):
         index = 0
         tab = list(repo.fetch())
@@ -81,6 +103,7 @@ class analyzePerceval(object):
             tabRes.append(tab[:index])
         return tabRes
 
+    # This method print an array in JSON format
     def printArray(self, arr):
         print("\nWORK IN PROGRESS !")
         try:
@@ -89,13 +112,33 @@ class analyzePerceval(object):
         except:
             print('\n')
 
+    # This method build a json with N messages
+    def buildJSON(self, repo):
+        base_path = os.path.dirname(os.path.realpath(__file__))
+        json_path = base_path.replace("/python/testPerceval/classes", "/data/json/")
+        n = input("\nType the number of messages you want to be saved : ")
+        n = int(n)
+        tab = list(repo.fetch())
+        index = 0
+        tabRes = []
+        for index in range(0, n):
+            index = index + 1
+            tabRes.append(tab[index])
+        index = 0
+        with open(json_path + 'json_' + str(n) + '_messages.json', 'w', encoding='utf-8') as f:
+            for index in range(0, n):
+                json.dump(tabRes[index], f, ensure_ascii=False, indent=4)
+        print("\nJSON created.")
+
+    # Introduction of the program
     def intro(self):
         print("This is a program who test Perceval.")
         print("\nYou will be asked to type a number corresponding of the action you want to do.")
         print("\nThe program will start to download the archive if not already downloaded.")
 
+    # Method who let the user choose an action
     def run(self, repo):
-        options = [0, 1, 2, 3, 4, 10]
+        options = [0, 1, 2, 3, 4, 5, 10]
         res = 0
         while res == 0:
             print("\nType the number of action you want to do.")
@@ -104,6 +147,7 @@ class analyzePerceval(object):
             print("2 : Count the mails send by someone")
             print("3 : Print the 5 firsts messages")
             print("4 : Search a keyword")
+            print("5 : Build a JSON with N messages")
             print("10 : Stop the program")
             res = input("\nYour choice : ")
             if int(res) == 1:
@@ -125,6 +169,9 @@ class analyzePerceval(object):
                     print("\nNo name specified, please retry")
                 else:
                     self.keyword(repo, key)
+                res = 0
+            if int(res) == 5:
+                self.buildJSON(repo)
                 res = 0
             if int(res) == 10:
                 print("\nEND")
