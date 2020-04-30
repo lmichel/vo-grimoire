eabe0e02eb413385e59433e46d252824037ecf42
cisterni2u <enzo.cisternino6@etu.univ-lorraine.fr>
2020-04-30 15:03:56 +0200

    30/04 : Proto Web

--- sandbox/web/ivoa_web/js/perso/research.js
+++ sandbox/web/ivoa_web/js/perso/research.js
@@ -2,6 +2,8 @@ function addSearchAttribute(input){
     let search_bar = $("#search_bar");
     search_bar.val(search_bar.val() + " " + input +" ")
     search_bar.focus();
+    var cursorPos = search_bar.prop('selectionStart');
+    search_bar.prop('selectionEnd',cursorPos - 2)
 }
 
 export default {
