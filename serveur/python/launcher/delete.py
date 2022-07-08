1192e11fc02811272efc58e9b5874dfe2823b20f
Laurent MICHEL <laurent.michel@astro.unistra.fr>
2022-07-08 18:17:18 +0200

    Create delete.py

---
+++ serveur/python/launcher/delete.py
@@ -0,0 +1,5 @@
+from elasticsearch import Elasticsearch
+es = Elasticsearch([{'host':'localhost', 'port':'9200'}])
+es.indices.delete(index='ivoa_all_temp_mails', ignore=[400, 404])
+es.indices.delete(index='ivoa_all_new_mails', ignore=[400, 404])
+es.indices.put_settings(index="_all", body={ "index.blocks.read_only_allow_delete": "false"})
